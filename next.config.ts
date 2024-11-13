655913997c42feb2c78e87e7bf4e668b04dd83ab
Hugodk33 <hugodk33@gmail.com>
2024-11-13 20:21:47 -0300

    gh-pages fixing 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: 'export',
+  assetPrefix: '/site-sstyle-switch',
+  basePath: '/site-sstyle-switch',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
